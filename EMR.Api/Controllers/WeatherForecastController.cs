e901f16912c93f0fa06389cb2dd601d584a823d9
Amr Rizk <amr.rizk@apcopay.com>
2024-12-15 16:17:29 +0200

    adding some changes

--- EMR.Api/Controllers/WeatherForecastController.cs
+++ EMR.Api/Controllers/WeatherForecastController.cs
@@ -13,6 +13,8 @@ namespace EMR.Api.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
+
+
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
