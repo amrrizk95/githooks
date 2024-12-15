80428571623bc779290a56b69bc15fbe32b41bb4
Amr Rizk <amr.rizk@apcopay.com>
2024-12-15 16:09:20 +0200

    Add project files.

---
+++ EMR.Test/CalculationTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMR.Test
+{
+    [TestClass]
+    public class CalculationTests
+    {
+
+        [TestMethod]
+        public void Calculate()
+        {
+            var result = 40;
+            // Assert
+            Assert.AreEqual(40, result);
+        }
+    }
+}
